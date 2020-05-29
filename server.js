b9fe81966ca3f8f35c18d8584f41e5eba88e01af
Deval Shah <HirDev@Devals-MacBook-Pro.local>
2020-05-29 17:16:57 -0400

    Server is up and running on port 4000

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+
+const app = express();
+
+const PORT = 4000;
+
+app.set('view-engine','ejs');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+app.listen('PORT', function(){
+    console.log(`Server is running on: ${PORT}`);
+});
