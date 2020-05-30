2edd3ea48abe6c774d9e901e61bb86b252eb01f7
ivoka <ivailo96g@gmail.com>
2020-05-30 13:02:06 -0400

    working Login / Logout / Create acc/ custom massage

--- controllers/user.js
+++ controllers/user.js
@@ -3,23 +3,25 @@ const router = express.Router()
 const db = require('../models')
 const bcrypt= require('bcryptjs');
 
-let userFlag=false , phoneFlag=false, loginFlag=false
+let userFlag=false , phoneFlag=false, loginFlag=false ,passwordFlag= false;
 
+// if  based on the session it will eather show him to log in or logout
 router.get('/' ,(req,res)=>{
-   console.log(req.session)
     res.render('user/index' ,  { user: req.session } )
 })
 
-
+// Creating a use 
 router.get('/create',  async (req,res)=>{
     
     res.render('user/newAcc' , {
         userFlag : userFlag,
-        phoneFlag : phoneFlag
+        phoneFlag : phoneFlag,
+        passwordFlag : passwordFlag
     })
 })
 
-
+// it Checks the DB  if it meets the requirements to Create a user
+// if it does then the user is created else it gives him a specific err 
 router.post('/create', async (req,res)=>{
 
 
@@ -27,26 +29,44 @@ router.post('/create', async (req,res)=>{
        const user = await db.User.findOne({Username: req.body.username})
        phoneFlag= false;
        userFlag= false;
+       passwordFlag= false
        
        if(user){
            userFlag = true 
            return res.redirect('/create')
         }
        const phone = await db.User.findOne({Phone: req.body.phone})
-       console.log(phone)
-       if(phone){
+       if(phone || req.body.phone.length <10 || req.body.phone.length>11){
            phoneFlag= true;
            return res.redirect('/create')
        }
        
+       if(req.body.password.length<3){
+        passwordFlag= true;
+        return res.redirect('/create')
+       }
         const salt = await bcrypt.genSalt(10)
         const hash =await bcrypt.hash(req.body.password ,salt)
         req.body.password=hash
+/// this is  fake DB feel free to delete it ! 
+        let car = Math.random()*100
+        if( car <10){car="toyota"}
+        else if( car <20){car="Ford"}
+        else if( car <30){car="Hunday"}
+        else if( car <40){car="lex"}
+        else if( car <50){car="ok"}
+        else if( car <60){car="Z"}
+        else if( car <70){car="X"}
+        else if( car <80){car="M"}
+        else if( car <100){car="F"}
+
 
        const creatingUser = {
+           
             Username : req.body.username,
             Password : req.body.password,
-            Phone : req.body.phone
+            Phone : req.body.phone,
+            Product : car
         }
         const newUser = await db.User.create(creatingUser)
         res.redirect('/')
@@ -64,7 +84,6 @@ router.get('/login', (req,res)=>{
 
 
 router.post('/login', async (req,res)=>{
-    console.log(req.body.username)
     try{
         loginFlag= false
         const foundUser = await db.User.findOne({Username: req.body.username})
@@ -74,7 +93,7 @@ router.post('/login', async (req,res)=>{
         const pass =await  bcrypt.compare(req.body.password , foundUser.Password)
         if(!pass){
             loginFlag=true;
-            return res.redirect('/login')}
+            return res.redirect('/login')}           
         req.session.currentUser= {
             id : foundUser._id ,
             username: foundUser.Username
@@ -82,9 +101,19 @@ router.post('/login', async (req,res)=>{
         res.redirect('/')
     }
     catch(err){
-        res.redirect('/create')
+        res.redirect('/login')
         console.log(err)
     }
+})
+
+
+router.post('/logout' , async (req,res)=>{
+    await req.session.destroy();
+    res.redirect('/')
+})
+
+router.get('/profile/:id' , async (req,res)=>{
+
 })
 
 module.exports= router
--- models/User.js
+++ models/User.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose')
 
 
+
+
+
 const userSchema = new mongoose.Schema({
     Username : {type: String , required:true , unique:true},
     Password : {type: String , minlength:3 , required:true},
     Phone: {type: Number , required: true , minlength:10 , maxlength:11 ,unique:true},
-    Products : {
+    Product : String
+    /*{
         type: mongoose.Schema.Types.ObjectId,
         ref : "Car"
-    }
+    }*/
 
 })
 
