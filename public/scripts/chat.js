b4293c846303fd503d6977f87ed4064444776203
ivoka <ivailo96g@gmail.com>
2020-06-05 01:09:16 -0400

    styles

---
+++ public/scripts/chat.js
@@ -0,0 +1,11 @@
+
+
+$('#buyertext').keypress(function(event){
+    if(event.keyCode==13)
+    {$('#buyersubmit').click()}
+})
+
+$('#sellertext').keypress(function(event){
+    if(event.keyCode==13)
+    {$('#sellersubmit').click()}
+})
