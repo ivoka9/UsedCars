5cb900098bf85d7070242153711cd25c094366d3
ivoka <ivailo96g@gmail.com>
2020-06-07 19:01:06 -0400

    sockets added

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,11 +1,43 @@
+let socket
+socket= io.connect('http://localhost:4000')
+socket.on('msg',addMsg)
 
+
+function addMsg(data){
+    if(data.seller==$("#sellerName").val() && data.buyer==$("#buyerName").val()){
+        $("#chat").append(`<p class="${data.who}">${data.msg} </p>`)
+    }
+    
+}
 
 $('#buyertext').keypress(function(event){
     if(event.keyCode==13)
-    {$('#buyersubmit').click()}
+   {   
+        let data = socketData (this.value,"buyer")
+        socket.emit('msg',data)
+        $('#buyersubmit').click()
+    }
 })
 
-$('#sellertext').keypress(function(event){
+$('#sellertext').keypress( function(event){
     if(event.keyCode==13)
-    {$('#sellersubmit').click()}
+    {
+    let data =  socketData (this.value,"seller")
+      socket.emit('msg',data)
+    $('#sellersubmit').click()
+    }
+
 })
+
+
+
+function socketData (MsgData,who){
+    let data= {
+        seller: $("#sellerName").val(),
+        buyer : $("#buyerName").val(),
+        msg :MsgData,
+        who: who
+    }
+    return data
+
+}
